34f7a1099ba97515038817758e514ce0684aa066
odilbek29 <kamoliddinpardaev272@gmail.com>
2025-02-20 19:17:33 +0500

    Add files via upload

---
+++ odil.js
@@ -0,0 +1,10 @@
+const a=prompt("string tybe malumot kiriting");
+const b=+prompt("nomber tybe malumot kiriting");
+const c=prompt("boolean tybe malumot kiriting");
+
+
+confirm("kiritgan narsalar tog`rimi");
+
+
+alert(`Siz kiritgan ${a} so'zi ${typeof a} type da \n    Siz kiritgan ${b} so'zi Boolean type da \n Siz kiritgan ${c} so'zi ${typeof c} type da \n
+    `)
